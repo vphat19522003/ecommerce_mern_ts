822995e392e7c98864473d5c0045a7eee0c73216
19522003 <19522003@gm.uit.edu.vn>
2024-09-24 16:54:47 +0700

    Add function navigate for mobile navigator

--- frontend/src/components/organisms/mobileNavigator/index.tsx
+++ frontend/src/components/organisms/mobileNavigator/index.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 import { ArrowBack, Home, Person } from '@mui/icons-material';
 import { Box, Stack } from '@mui/material';
@@ -6,11 +6,12 @@ import { Box, Stack } from '@mui/material';
 import { paths } from '@app/routes/paths';
 
 const MobileNavigator = (): JSX.Element => {
+  const navigate = useNavigate();
   return (
     <Box className='fixed bottom-0 left-0 z-30 w-full bg-white' sx={{ boxShadow: '0 -4px 6px rgba(0, 0, 0, 0.05)' }}>
       <Stack direction={'row'} justifyContent={'space-between'} className='px-4 py-2'>
         <Box>
-          <ArrowBack />
+          <ArrowBack onClick={() => navigate(-1)} className='hover:cursor-pointer' />
         </Box>
         <Box>
           <Link to={paths.index}>
