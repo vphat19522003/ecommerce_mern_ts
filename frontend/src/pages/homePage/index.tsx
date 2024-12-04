698aea57f5fb696a9b48b89acb5749e616691457
19522003 <19522003@gm.uit.edu.vn>
2024-12-04 11:13:59 +0700

    Add image viewer in product detail

--- frontend/src/pages/homePage/index.tsx
+++ frontend/src/pages/homePage/index.tsx
@@ -1,8 +1,5 @@
-import { useState } from 'react';
+import { Stack } from '@mui/material';
 
-import { Box, Grid, Stack } from '@mui/material';
-
-import ImageViewer from '@app/components/organisms/imageViewer';
 import MainBanner from '@app/components/organisms/mainBanner';
 import { useDevice } from '@app/hooks/useDevice';
 
@@ -11,86 +8,22 @@ import VideoBanner from './components/VideoBanner';
 
 const HomePage = (): JSX.Element => {
   const { isMobile } = useDevice();
-  const [isViewerOpen, setViewerOpen] = useState(false);
-  const [viewerIndex, setViewerIndex] = useState(0);
 
-  const images = [
-    'https://via.placeholder.com/800x600?text=Image+1',
-    'https://via.placeholder.com/800x600?text=Image+2',
-    'https://via.placeholder.com/800x600?text=Image+3',
-    'https://via.placeholder.com/800x600?text=Image+4',
-    'https://via.placeholder.com/800x600?text=Image+5',
-    'https://via.placeholder.com/800x600?text=Image+6',
-    'https://via.placeholder.com/800x600?text=Image+7',
-    'https://via.placeholder.com/800x600?text=Image+8',
-    'https://via.placeholder.com/800x600?text=Image+9',
-    'https://via.placeholder.com/800x600?text=Image+10',
-    'https://via.placeholder.com/800x600?text=Image+11',
-    'https://via.placeholder.com/800x600?text=Image+12',
-    'https://via.placeholder.com/800x600?text=Image+13',
-    'https://via.placeholder.com/800x600?text=Image+14',
-    'https://via.placeholder.com/800x600?text=Image+15',
-    'https://via.placeholder.com/800x600?text=Image+16',
-    'https://via.placeholder.com/800x600?text=Image+17',
-    'https://via.placeholder.com/800x600?text=Image+18',
-    'https://via.placeholder.com/800x600?text=Image+19',
-    'https://via.placeholder.com/800x600?text=Image+20',
-    'https://via.placeholder.com/800x600?text=Image+21',
-    'https://via.placeholder.com/800x600?text=Image+22',
-    'https://via.placeholder.com/800x600?text=Image+23',
-    'https://via.placeholder.com/800x600?text=Image+24'
-  ];
-
-  const handleImageClick = (index: number) => {
-    setViewerIndex(index); // Ghi lại chỉ số ảnh được click
-    setViewerOpen(true); // Mở ImageViewer
-  };
   return (
     <>
       <MainBanner />
       <Stack className={`${isMobile && 'pl-2'} py-6 `}>
         <ListProductBook />
         <ListProductBook />
-        <Grid container spacing={2}>
-          {images.map((image, index) => (
-            <Grid
-              item
-              xs={6}
-              sm={3}
-              key={index}
-              onClick={() => handleImageClick(index)} // Bắt sự kiện click
-              sx={{ cursor: 'pointer' }}>
-              <Box
-                component='img'
-                src={image}
-                alt={`Image ${index}`}
-                sx={{
-                  width: '100%',
-                  height: '150px',
-                  objectFit: 'cover',
-                  borderRadius: '8px',
-                  transition: '0.3s',
-                  '&:hover': {
-                    transform: 'scale(1.05)'
-                  }
-                }}
-              />
-            </Grid>
-          ))}
-        </Grid>
 
-        {/* Hiển thị ImageViewer khi người dùng click */}
-        {isViewerOpen && (
-          <ImageViewer images={images} initialIndex={viewerIndex} onClose={() => setViewerOpen(false)} />
-        )}
         {/* <Typography>KHUYỂN MÃI SHOCK NHẤT</Typography>
-    <Typography>TOP SẢN PHẨM BÁN CHẠY</Typography>
-    <Typography>SẢN PHẨM MỚI</Typography>
-    <Typography>BẠN CÓ THỂ THÍCH</Typography>
-    <Typography>TOYS</Typography>
-    <Typography>GAME</Typography>
-    <Typography>ELECTRONICS</Typography>
-    <Typography>FOOD</Typography> */}
+            <Typography>TOP SẢN PHẨM BÁN CHẠY</Typography>
+            <Typography>SẢN PHẨM MỚI</Typography>
+            <Typography>BẠN CÓ THỂ THÍCH</Typography>
+            <Typography>TOYS</Typography>
+            <Typography>GAME</Typography>
+            <Typography>ELECTRONICS</Typography>
+            <Typography>FOOD</Typography> */}
 
         {/* <WhyChooseUsBanner /> */}
         {/* <ParallaxBanner /> */}
--- frontend/src/pages/productDetail/components/ProductImage.tsx
+++ frontend/src/pages/productDetail/components/ProductImage.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 
 import { Box, Divider, Stack } from '@mui/material';
 
+import ImageViewer from '@app/components/organisms/imageViewer';
 import { getProductDetailCustom } from '@app/pages/admin/ecommerce/addNewProductPage/components/schemas';
 
 type ProductImageProps = {
@@ -10,6 +11,8 @@ type ProductImageProps = {
 const ProductImage = ({ productDetail }: ProductImageProps): JSX.Element => {
   const [selectedImage, setSelectedImage] = useState<string>('');
   const [fade, setFade] = useState<boolean>(false);
+  const [isViewerOpen, setViewerOpen] = useState(false);
+  const [viewerIndex, setViewerIndex] = useState(0);
 
   const handleImageChange = (imageUrl: string) => {
     setFade(true);
@@ -22,49 +25,66 @@ const ProductImage = ({ productDetail }: ProductImageProps): JSX.Element => {
   useEffect(() => {
     setSelectedImage(productDetail?.productThumbImg.url);
   }, [productDetail]);
+
+  const handleImageClick = (index: number) => {
+    setViewerIndex(index);
+    setViewerOpen(true);
+  };
+
+  const imageArrTemp = [
+    productDetail?.productThumbImg?.url,
+    ...(productDetail?.productDescImg.map((image) => image.url) || [])
+  ];
   return (
-    <Stack direction={'column'} className='max-h-max'>
-      <Stack className='px-4 py-2'>
-        {/* Main Image */}
-        <Stack className='h-[368px] border-[0.2px] border-solid border-slate-200 rounded-lg'>
-          <img
-            src={selectedImage}
-            alt={productDetail?.productName}
-            className={`w-full h-full object-contain transition-opacity duration-200 ${fade ? 'opacity-0' : 'opacity-100'}`}
-          />
-        </Stack>
-        {/* List Image */}
-        <Stack direction={'row'} spacing={2} className='pt-2'>
-          <Box
-            className={`cursor-pointer size-16 border-[0.2px] border-solid border-slate-200 rounded-lg overflow-hidden ${selectedImage === productDetail?.productThumbImg.url ? 'border-blue-700 border-[1.4px]' : ' border-[0.2px]  border-slate-200'}`}
-            onClick={() => handleImageChange(productDetail?.productThumbImg.url)}>
+    <>
+      <Stack direction={'column'} className='max-h-max'>
+        <Stack className='px-4 py-2'>
+          {/* Main Image */}
+          <Stack
+            className='h-[368px] border-[0.2px] border-solid border-slate-200 rounded-lg'
+            onClick={() => handleImageClick(imageArrTemp.findIndex((item) => item === selectedImage))}>
             <img
-              src={productDetail?.productThumbImg.url}
+              src={selectedImage}
               alt={productDetail?.productName}
-              className='w-full h-full object-contain'
+              className={`w-full h-full object-contain transition-opacity duration-200 ${fade ? 'opacity-0' : 'opacity-100'}`}
             />
-          </Box>
-          {productDetail?.productDescImg.length > 0 &&
-            productDetail.productDescImg.map((productDescImage, index) => (
-              <Box
-                className={`cursor-pointer size-16 border-solid rounded-lg overflow-hidden ${selectedImage === productDescImage.url ? 'border-blue-700 border-[1.4px]' : ' border-[0.2px]  border-slate-200'}`}
-                key={index}
-                onClick={() => handleImageChange(productDescImage.url)}>
-                <img
-                  src={productDescImage.url}
-                  alt={productDetail?.productName}
-                  className='w-full h-full object-contain'
-                />
-              </Box>
-            ))}
+          </Stack>
+          {/* List Image */}
+          <Stack direction={'row'} spacing={2} className='pt-2'>
+            <Box
+              className={`cursor-pointer size-16 border-[0.2px] border-solid border-slate-200 rounded-lg overflow-hidden ${selectedImage === productDetail?.productThumbImg.url ? 'border-blue-700 border-[1.4px]' : ' border-[0.2px]  border-slate-200'}`}
+              onClick={() => handleImageChange(productDetail?.productThumbImg.url)}>
+              <img
+                src={productDetail?.productThumbImg.url}
+                alt={productDetail?.productName}
+                className='w-full h-full object-contain'
+              />
+            </Box>
+            {productDetail?.productDescImg.length > 0 &&
+              productDetail.productDescImg.map((productDescImage, index) => (
+                <Box
+                  className={`cursor-pointer size-16 border-solid rounded-lg overflow-hidden ${selectedImage === productDescImage.url ? 'border-blue-700 border-[1.4px]' : ' border-[0.2px]  border-slate-200'}`}
+                  key={index}
+                  onClick={() => handleImageChange(productDescImage.url)}>
+                  <img
+                    src={productDescImage.url}
+                    alt={productDetail?.productName}
+                    className='w-full h-full object-contain'
+                  />
+                </Box>
+              ))}
+          </Stack>
+        </Stack>
+        <Divider />
+        {/* Some Action */}
+        <Stack>
+          <p className='px-4'>Xem tóm tắt</p>
         </Stack>
       </Stack>
-      <Divider />
-      {/* Some Action */}
-      <Stack>
-        <p className='px-4'>Xem tóm tắt</p>
-      </Stack>
-    </Stack>
+      {isViewerOpen && (
+        <ImageViewer images={imageArrTemp} initialIndex={viewerIndex} onClose={() => setViewerOpen(false)} />
+      )}
+    </>
   );
 };
 
