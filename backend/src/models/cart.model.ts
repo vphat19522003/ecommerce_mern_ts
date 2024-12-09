c9986e4ddf8fa38db1920364f63e7ee36d71b542
19522003 <19522003@gm.uit.edu.vn>
2024-12-09 14:02:58 +0700

    Create Model Cart, Order

---
+++ backend/src/models/cart.model.ts
@@ -0,0 +1,41 @@
+import mongoose, { model, Schema } from 'mongoose';
+
+export interface CartItem {
+  productId: mongoose.Types.ObjectId;
+  quantity: number;
+}
+
+export interface ICart extends Document {
+  userId: mongoose.Types.ObjectId;
+  cartItems: CartItem[];
+}
+
+const CartItemSchema = new Schema<CartItem>(
+  {
+    productId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Product',
+      required: true
+    },
+    quantity: { type: Number, required: true, min: 1 }
+  },
+  {
+    _id: false
+  }
+);
+
+const CartSchema = new Schema<ICart>({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  cartItems: {
+    type: [CartItemSchema],
+    default: []
+  }
+});
+
+const CartModel = model<ICart>('Cart', CartSchema);
+
+export default CartModel;
