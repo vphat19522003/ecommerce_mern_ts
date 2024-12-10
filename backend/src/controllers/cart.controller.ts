beea591a415a1debf1a890925dd19fd8869ed6d1
19522003 <19522003@gm.uit.edu.vn>
2024-12-10 10:42:28 +0700

    create cart api

--- backend/src/controllers/cart.controller.ts
+++ backend/src/controllers/cart.controller.ts
@@ -12,3 +12,41 @@ export const addToCartController = async (req: Request, res: Response): Promise<
     result
   });
 };
+
+export const updateCartQuantityController = async (req: Request, res: Response): Promise<Response> => {
+  const result = await CartService.updateCartQuantity(req);
+
+  return res.json({
+    status: STATUS_CODE.OK,
+    message: 'Quantity successfully updated',
+    result
+  });
+};
+
+export const removeProductCartController = async (req: Request, res: Response): Promise<Response> => {
+  await CartService.removeProductCart(req);
+
+  return res.json({
+    status: STATUS_CODE.OK,
+    message: 'Remove item from cart successfully'
+  });
+};
+
+export const removeAllProductCartController = async (req: Request, res: Response): Promise<Response> => {
+  await CartService.removeAllProductCart(req);
+
+  return res.json({
+    status: STATUS_CODE.OK,
+    message: 'Delete cart successfully'
+  });
+};
+
+export const getMyCartController = async (req: Request, res: Response): Promise<Response> => {
+  const result = await CartService.getMyCart(req);
+
+  return res.json({
+    status: STATUS_CODE.OK,
+    message: 'Get cart successfully',
+    result
+  });
+};
--- backend/src/services/cart.service.ts
+++ backend/src/services/cart.service.ts
@@ -3,7 +3,7 @@ import { Types } from 'mongoose';
 import STATUS_CODE from '@app/constants/responseStatus';
 import { CustomError } from '@app/core/response.error';
 import { IRequestCustom } from '@app/middleware/accessToken.middleware';
-import CartModel from '@app/models/cart.model';
+import CartModel, { ICart } from '@app/models/cart.model';
 import ProductModel from '@app/models/product.model';
 import { UserInfo } from '@app/repository/user.repository';
 
@@ -40,10 +40,97 @@ class CartService {
     }
 
     await cart.save();
+  }
 
-    await ProductModel.findByIdAndUpdate(product._id, {
-      availableQuantity: product.availableQuantity - quantity
-    });
+  static async updateCartQuantity(req: IRequestCustom): Promise<ICart> {
+    const { _id: userId } = req.user as UserInfo;
+    const data = req.body;
+
+    const quantity = Number(data.quantity);
+    const productId = data.productId;
+
+    if (!Types.ObjectId.isValid(productId)) {
+      throw new CustomError('Product ID is not valid', STATUS_CODE.BAD_REQUEST);
+    }
+
+    if (quantity == 0) {
+      throw new CustomError('Quantity must be greater or less than zero', STATUS_CODE.BAD_REQUEST);
+    }
+
+    const product = await ProductModel.findById(productId);
+
+    if (!product) throw new CustomError('Product not found', STATUS_CODE.BAD_REQUEST);
+
+    const cart = await CartModel.findOne({ userId });
+
+    if (!cart) throw new CustomError('Cart not found', STATUS_CODE.BAD_REQUEST);
+
+    const existingItem = cart.cartItems.find((item) => String(item.productId) === String(product._id));
+
+    if (!existingItem) throw new CustomError('Not found product in cart', STATUS_CODE.BAD_REQUEST);
+
+    if (Number(product.availableQuantity) < quantity)
+      throw new CustomError('Available quantity is not enough', STATUS_CODE.BAD_REQUEST);
+
+    const tempQuantity = Number(existingItem.quantity) + quantity;
+
+    if (tempQuantity <= 0) throw new CustomError('Can not decrease quantity', STATUS_CODE.BAD_REQUEST);
+
+    if (tempQuantity > Number(product.availableQuantity))
+      throw new CustomError('Cart item quantity is pass available quantity', STATUS_CODE.BAD_REQUEST);
+
+    existingItem.quantity = tempQuantity;
+
+    const updatedCart = await cart.save();
+
+    return updatedCart;
+  }
+
+  static async removeProductCart(req: IRequestCustom): Promise<void> {
+    const { _id: userId } = req.user as UserInfo;
+    const { productId } = req.body;
+
+    if (!Types.ObjectId.isValid(productId)) {
+      throw new CustomError('Product ID is not valid', STATUS_CODE.BAD_REQUEST);
+    }
+
+    const product = await ProductModel.findById(productId);
+
+    if (!product) throw new CustomError('Product not found', STATUS_CODE.BAD_REQUEST);
+
+    const cart = await CartModel.findOne({ userId });
+
+    if (!cart) throw new CustomError('Cart not found', STATUS_CODE.BAD_REQUEST);
+
+    const existingItem = cart.cartItems.find((item) => String(item.productId) === String(product._id));
+
+    if (!existingItem) throw new CustomError('Not found product in cart', STATUS_CODE.BAD_REQUEST);
+
+    cart.cartItems = cart.cartItems.filter((item) => String(item.productId) !== String(product._id));
+
+    await cart.save();
+  }
+
+  static async removeAllProductCart(req: IRequestCustom): Promise<void> {
+    const { _id: userId } = req.user as UserInfo;
+
+    const cart = await CartModel.findOne({ userId });
+
+    if (!cart) throw new CustomError('Cart not found', STATUS_CODE.BAD_REQUEST);
+
+    await CartModel.deleteOne({ _id: cart._id });
+  }
+
+  static async getMyCart(req: IRequestCustom): Promise<{ myCart: ICart; totalCartItem: number }> {
+    const { _id: userId } = req.user as UserInfo;
+
+    const myCart = await CartModel.findOne({ userId }).populate('cartItems.productId');
+
+    if (!myCart) throw new CustomError('Cart not found', STATUS_CODE.BAD_REQUEST);
+
+    const totalCartItem = myCart.cartItems.reduce((acc, cur) => acc + cur.quantity, 0);
+
+    return { myCart, totalCartItem };
   }
 }
 
